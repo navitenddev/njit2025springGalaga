b6773b2d42f75cb1a5aa7289ad4ba9a9bd647a78
Ryan Kulfan <ryan@kulfan.org>
2025-03-08 10:52:37 -0500

    adjusted enemy turning during pathing

--- src/main.js
+++ src/main.js
@@ -1,10 +1,178 @@
 import './style.css'
 import Phaser from 'phaser'
-   
+
 const sizes = {
   width: 500,
   height: 500
 };
+
+class Enemy extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, texture, movePattern, yLevel, index) {
+    super(scene, x, y, texture);
+
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+    this.setScale(0.5);
+    this.setOrigin(0.5);
+    this.body.setAllowGravity(false);
+    this.body.moves = false;
+
+    this.movePattern = movePattern;
+    this.followingGroup = true;
+    this.yLevel = yLevel;
+    this.index = index;
+    this.targetX = x;
+    this.targetY = y;
+    this.reachedFinalPosition = false;
+  }
+
+  moveTo(x, y) {
+    this.scene.tweens.add({
+      targets: this,
+      x: x,
+      y: y,
+      ease: 'linear',
+      duration: 500,
+      onComplete: () => {
+        this.reachedFinalPosition = true;
+        this.followingGroup = true;
+        this.targetX = x;
+      }
+    });
+  }
+}
+
+// Enemies class represents a group of enemy objects
+class Enemies extends Phaser.Physics.Arcade.Group {
+  constructor(scene) {
+    super(scene.physics.world, scene);
+    this.scene = scene;
+    this.groupCenter = { x: 250, y: 150 }; // Fixed center point of the group
+    this.enemyGap = 28;
+    this.timeElapsed = 0;
+
+    this.createEnemyGrid();  // Initialize enemy grid
+
+    this.scene.time.addEvent({
+      delay: 400,
+      callback: this.updateGroupMovement,
+      callbackScope: this,
+      loop: true,
+    });
+  }
+
+  createEnemyGrid() {
+    this.enemiesData = [
+      { y: 0, type: "enemy1", count: 10, movePattern: "a" },
+      { y: 1, type: "enemy1", count: 10, movePattern: "a" },
+      { y: 2, type: "enemy2", count: 8, movePattern: "b" },
+      { y: 3, type: "enemy2", count: 8, movePattern: "b" },
+      { y: 4, type: "enemy3", count: 4, movePattern: "c" }
+    ];
+
+    this.enemiesData.forEach((levelData) => {
+      const levelY = levelData.y === 0 ? -50 : this.groupCenter.y - levelData.y * this.enemyGap;
+      const totalWidth = (levelData.count - 1) * this.enemyGap;
+      const startX = this.groupCenter.x - totalWidth / 2;
+
+      for (let i = 0; i < levelData.count; i++) {
+        const enemy = new Enemy(
+          this.scene,
+          startX + i * this.enemyGap,
+          levelY,
+          levelData.type,
+          levelData.movePattern,
+          levelData.y,
+          i
+        );
+
+        this.add(enemy);
+      }
+    });
+
+    this.initiatePathMovement();
+  }
+
+  initiatePathMovement() {
+    const path = new Phaser.Curves.Path(75, -50);
+    path.splineTo([
+      100, 50, 50, 100, 100, 150, 50, 200,
+      100, 250, 50, 300, 100, 350, 200, 300,
+      250, 250, 200, 200
+    ]);
+
+    this.getChildren().splice(0, 10).forEach((enemy, index) => {
+      enemy.followingGroup = false;
+      this.scene.tweens.addCounter({
+        from: 0,
+        to: 1,
+        ease: 'linear',
+        duration: 5000,
+        delay: 400 * index,
+        repeat: 0,
+        onUpdate: (tween) => {
+          const t = tween.getValue();
+          const position = path.getPoint(t);
+          const tangent = path.getTangent(t);
+
+          if (position) {
+            enemy.setPosition(position.x, position.y);
+          }
+          // Turn throughout the path
+          if (tangent) {
+            const angle = Math.atan2(tangent.y, tangent.x) + Math.PI / 2;
+            enemy.setRotation(angle);
+          } 
+        },
+        onComplete: () => {
+          this.moveToFinalGridPosition(enemy);
+          enemy.followingGroup = true;
+        }
+      });
+    });
+  }
+
+  moveToFinalGridPosition(enemy) {
+    const levelData = this.enemiesData[enemy.yLevel];
+
+    const totalWidth = (levelData.count - 1) * this.enemyGap;
+    const startX = this.groupCenter.x - totalWidth / 2;
+    const targetX = startX + enemy.index * this.enemyGap;
+    const targetY = this.groupCenter.y - enemy.yLevel * this.enemyGap;
+
+    // Move enemy to final grid position
+    this.scene.tweens.add({
+      targets: enemy,
+      x: targetX,
+      y: targetY,
+      ease: 'linear',
+      duration: 500,
+      onComplete: () => {
+        enemy.reachedFinalPosition = true;
+        enemy.followingGroup = true; // Re-enable formation movement
+        enemy.targetX = targetX;
+
+        // Ensure the enemy is part of the oscillating movement
+        if (!this.getChildren().includes(enemy)) {
+          this.add(enemy);
+        }
+        enemy.setRotation(0);
+      }
+    });
+  }
+
+  updateGroupMovement() {
+    this.timeElapsed += 0.05;
+    const offset = 50 * Math.sin(this.timeElapsed);
+
+    this.getChildren().forEach((enemy) => {
+      if (!enemy.followingGroup) return;
+
+      enemy.targetX = enemy.targetX || enemy.x;
+      enemy.x = enemy.targetX + offset;
+    });
+  }
+}
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -22,143 +190,7 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
-    this.groupCenter = { x: 250, y: 150 }; // Fixed center point of the group
-
-    // Create enemies in the group
-    this.createEnemies(0, "a", "enemy1", 10);
-    this.createEnemies(1, "a", "enemy1", 10);
-    this.createEnemies(2, "b", "enemy2", 8);
-    this.createEnemies(3, "b", "enemy2", 8);
-    this.createEnemies(4, "c", "enemy3", 4);
-
-    // console.log("Total enemies in enemySprites:", this.enemySprites.length);
-    // this.enemySprites.forEach((e, i) => console.log(`Enemy ${i} - followingGroup: ${e.followingGroup}`));
-
-    this.enemySprites = [];
-    this.enemies.forEach((levelData) => {
-      const { y, enemiesAtLevel } = levelData;
-      const levelY = (y == 0) ? -50 : this.groupCenter.y - y * this.enemyGap;
-      const totalWidth = (enemiesAtLevel.length - 1) * this.enemyGap;
-      const startX = this.groupCenter.x - totalWidth / 2;
-
-      enemiesAtLevel.forEach((data, index) => {
-        let enemy = this.physics.add.sprite(
-          startX + index * this.enemyGap,
-          levelY,
-          data.type
-        ).setScale(0.5);
-
-        enemy.setOrigin(0.5);
-        enemy.body.setAllowGravity(false);
-        enemy.body.moves = false;
-
-        enemy.movePattern = data.movePattern;
-        enemy.followingGroup = true;
-        enemy.yLevel = y;
-        enemy.index = index;
-        enemy.targetX = startX + index * this.enemyGap;
-        enemy.targetY = levelY;
-
-        this.enemySprites.push(enemy);
-      });
-    });
-
-    // Initial enemy path movement
-    const path = new Phaser.Curves.Path(75, -50);
-    path.splineTo([100, 50, 50, 100, 100, 150, 50, 200, 100, 250, 50, 300, 100, 350]);
-    path.splineTo([200, 300, 250, 250, 200, 200]);
-
-    this.enemySprites.splice(0, 10).forEach((enemy, index) => {
-      enemy.followingGroup = false;
-      this.tweens.addCounter({
-        from: 0,
-        to: 1,
-        ease: 'linear',
-        duration: 5000,
-        delay: 400 * index,
-        repeat: 0,
-        onUpdate: tween => {
-          const t = tween.getValue();
-          const position = path.getPoint(t);
-          const tangent = path.getTangent(t);
-
-          if (position) {
-            enemy.setPosition(position.x, position.y);
-          }
-
-          if (tangent) {
-            const angle = Math.atan2(tangent.y, tangent.x);
-            enemy.setRotation(angle);
-          }
-        },
-        onComplete: () => {
-          this.moveToFinalGridPosition(enemy);
-        },
-      });
-    });
-
-    // Group movement using update loop
-    this.time.addEvent({
-      delay: 400,
-      callback: this.updateGroupMovement,
-      callbackScope: this,
-      loop: true,
-    });
-  }
-
-  updateGroupMovement() {
-    this.timeElapsed += 0.05;
-    const offset = 50 * Math.sin(this.timeElapsed);
-
-    this.enemySprites.forEach((enemy) => {
-      if (!enemy.followingGroup) return;
-
-      enemy.targetX = enemy.targetX || enemy.x;
-      enemy.x = enemy.targetX + offset;
-    });
-
-    // console.log(`Group moving: Offset ${offset}`);
-  }
-
-  // Split off at the end of the path to grid location
-  moveToFinalGridPosition(enemy) {
-    const levelData = this.enemies[enemy.yLevel];
-
-    const totalWidth = (levelData.enemiesAtLevel.length - 1) * this.enemyGap;
-    const startX = this.groupCenter.x - totalWidth / 2;
-    const targetX = startX + enemy.index * this.enemyGap;
-    const targetY = this.groupCenter.y - enemy.yLevel * this.enemyGap;
-
-    this.tweens.add({
-      targets: enemy,
-      x: targetX,
-      y: targetY,
-      ease: 'linear',
-      duration: 500,
-      onComplete: () => {
-        enemy.reachedFinalPosition = true;
-        enemy.followingGroup = true;
-        enemy.targetX = targetX;
-        if (!this.enemySprites.includes(enemy)) {
-          this.enemySprites.push(enemy);
-        }
-      }
-    });
-  }
-
-  createEnemies(y, movePattern, type, count) {
-    let existingLevel = this.enemies.find(level => level.y === y);
-
-    if (existingLevel) {  // Enemies already on that level
-      existingLevel.enemiesAtLevel.push(
-        ...Array.from({ length: count }, () => ({ movePattern, type }))
-      );
-    } else {  // Enemies on a new level
-      this.enemies.push({
-        y: y,
-        enemiesAtLevel: Array.from({ length: count }, () => ({ movePattern, type }))
-      });
-    }
+    this.enemies = new Enemies(this);
   }
 }
 
