6e7e76fc28599d5c813ced669c935d4a702206c7
Ryan Kulfan <ryan@kulfan.org>
2025-04-05 14:39:52 -0400

    created startDive functionality for enemies

--- src/objects/Enemies.js
+++ src/objects/Enemies.js
@@ -1,22 +1,40 @@
 import Phaser from 'phaser';
+import { sizes } from '../config';
 
 class Enemy extends Phaser.Physics.Arcade.Sprite {
     
-    constructor(scene, x, y, texture, movePattern, yLevel, index) {
+    constructor(scene, x, y, texture, movePattern, yLevel, index, enemyGroup) {
         super(scene, x, y, texture);
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.setScale(0.5);
         this.setOrigin(0.5);
         this.body.moves = false;
+        this.isDiving = false;  // ADDED: isDiving to avoid calling startDiving on an already diving enemy
 
         this.movePattern = movePattern;
         this.yLevel = yLevel;
         this.index = index;
+        this.enemyGroup = enemyGroup;
     }
 
     hits() {
         this.destroy();
+    }
+
+    // ADDED: startDive defines the diving path and function
+    startDive() {
+        this.isDiving = true;
+        const divePath = new Phaser.Curves.Path(this.x, this.y);
+        divePath.splineTo([100, 550, 230, 450, 250, 400]);
+        divePath.circleTo(50, true, 0);
+        divePath.ellipseTo(90, 300, 300, 60, false);
+
+        createTween(this.scene, this, divePath, 0, 5000, () => {
+            if (this.y > sizes.height) this.y = -50
+            this.enemyGroup.moveToFinalGridPosition(this);
+            this.isDiving = false;
+        });
     }
 }
 
@@ -27,16 +45,17 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
         this.groupCenter = { x: 550 / 2, y: 200 };  // Fixed center point of the group
         this.enemyGap = 37;
         this.timeElapsed = 0;
-        this.isbreathing = true;
+        this.isbreathing = false;   // CHANGED: Disabled breathing for testing
 
         this.createEnemyGrid(); // Initialize enemy grid
         
+        /* CHANGED: Disabled group movement for testing
         this.scene.time.addEvent({
             delay: 300,
             callback: this.updateGroupMovement,
             callbackScope: this,
             loop: true,
-        });
+        });*/
     }
 
     createEnemyGrid() {
@@ -61,7 +80,8 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
                     levelData.type,
                     levelData.movePattern,
                     levelData.y,
-                    i
+                    i,
+                    this
                 );
 
                 this.add(enemy);
@@ -69,6 +89,9 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
         });
 
         this.initiatePathMovement();
+        this.scene.time.delayedCall(5500, () => {
+            this.getChildren()[0].startDive();
+        });
     }
 
     initiatePathMovement() {
@@ -97,44 +120,22 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
 
         // Left side entry
         leftGroup.forEach((enemy, index) => {
-            tweens.push(this.createTween(enemy, leftEntry, index));
+            tweens.push(createTween(this.scene, enemy, leftEntry, index, 3000, () => {
+                this.moveToFinalGridPosition(enemy);
+            }));
         });
 
         // Right side entry
         rightGroup.forEach((enemy, index) => {
-            tweens.push(this.createTween(enemy, rightEntry, index));
+            tweens.push(createTween(this.scene, enemy, rightEntry, index, 3000, () => {
+                this.moveToFinalGridPosition(enemy);
+            }));
         });
 
         // Start both left & right tweens at the same time
         this.scene.tweens.add({
             targets: tweens,
             delay: 0
-        });
-    }
-
-    createTween(enemy, path, index = 1) {
-        return this.scene.tweens.addCounter({
-            from: 0,
-            to: 1,
-            duration: 3000,
-            delay: 120 * index,
-            onUpdate: (tween) => {
-                const t = tween.getValue();
-                const position = path.getPoint(t);
-                const tangent = path.getTangent(t);
-
-                if (position) {
-                    enemy.setPosition(position.x, position.y);
-                }
-                // Turn throughout the path
-                if (tangent) {
-                    const angle = Math.atan2(tangent.y, tangent.x) + Math.PI / 2;
-                    enemy.setRotation(angle);
-                }
-            },
-            onComplete: () => {
-                this.moveToFinalGridPosition(enemy);
-            }
         });
     }
 
@@ -146,15 +147,24 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
         let targetX = offset + startX + enemy.index * this.enemyGap;
         if (this.isbreathing) targetX = enemy.x + ((targetX - 275) * (offset / 3000));
         const targetY = this.groupCenter.y - enemy.yLevel * this.enemyGap;
-
-        this.scene.tweens.add({
-            targets: enemy,
-            x: targetX,
-            y: targetY,
-            ease: 'linear',
-            duration: 1000
-        });
+    
+        // CHANGED: Adjusted spline creation to fit with createTween
+        // NOTE: Needs to be cleaned up, and can probably use createTween for all
+        if (enemy.isDiving) {
+            const path = new Phaser.Curves.Path(enemy.x, enemy.y);
+            path.splineTo([targetX, targetY]);
+            createTween(this.scene, enemy, path, enemy.index, 1000, () => {enemy.setRotation(0)});
+        } else {
+            this.scene.tweens.add({
+                targets: enemy,
+                x: targetX,
+                y: targetY,
+                ease: 'linear',
+                duration: 1000
+            });
+        }
     }
+    
 
     updateGroupMovement() {
         const offset = 50 * Math.sin(this.timeElapsed);
@@ -169,7 +179,32 @@ export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
 
             if (this.isbreathing) enemy.x = enemy.x + ((targetX - 275) * (offset / 3000));
             else enemy.x = targetX + offset;
-            console.log("start: ", targetX, "x: ", enemy.x);
+            // console.log("start: ", targetX, "x: ", enemy.x);
         });
     }
+}
+
+// CHANGED: extracted createTween to be used by both Enemy and EnemyGroup
+export function createTween(scene, enemy, path, index = 0, duration, onComplete = () => {}) {
+    return scene.tweens.addCounter({
+        from: 0,
+        to: 1,
+        duration: duration,
+        delay: 120 * index,
+        onUpdate: (tween) => {
+            const t = tween.getValue();
+            const position = path.getPoint(t);
+            const tangent = path.getTangent(t);
+
+            if (position) {
+                enemy.setPosition(position.x, position.y);
+            }
+
+            if (tangent) {
+                const angle = Math.atan2(tangent.y, tangent.x) + Math.PI / 2;
+                enemy.setRotation(angle);
+            }
+        },
+        onComplete: onComplete
+    });
 }
